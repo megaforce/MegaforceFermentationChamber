c5c0772db6b95990ed5568f0d9660c69a7054fe8
Matjaž <matjaz.zibert2@gmail.com>
2020-07-22 15:35:33 +0200

    Swaped Timer1 with Timer3
    I need Timer1 for ADC

--- Code/MegaforceFermentationChamber/MegaforceFermentationChamber/TimeHandler.c
+++ Code/MegaforceFermentationChamber/MegaforceFermentationChamber/TimeHandler.c
@@ -10,16 +10,16 @@
 
 void init_TH()
 {
-	//Timer1 is in charge of keeping track of time remaining for the fermentation process 
-	TCCR1A = (1 << WGM12); // Enables Timer1 to run in CTC mode
-	OCR1A = 62500; // Number of ticks required to have timer keep track of time in seconds 
-	TCCR1A 	= (0 << COM1A1) | (0 << COM1A0); //Sets normal port operation, OC1A disconnected
-	//OC1A is disconnected since TH doesn't require an external output
-	TCCR1B = (1 << CS12); // Sets the pre-scaler to /256	
-	TIMSK1 = (1 << OCIE1A); // Enables the output compare A
+	//Timer3 is in charge of keeping track of time remaining for the fermentation process 
+	TCCR3A = (1 << WGM32); // Enables Timer3 to run in CTC mode
+	OCR3A = 62500; // Number of ticks required to have timer keep track of time in seconds
+	TCCR3A 	= (0 << COM3A1) | (0 << COM3A0); //Sets normal port operation, OC3A disconnected
+	//OC3A is disconnected since TH doesn't require an external output
+	TCCR3B = (1 << CS32); // Sets the pre-scaler to /256
+	TIMSK3 = (1 << OCIE3A); // Enables the output compare A
 }
 
 void disable_TH()
 {
-	TCCR1B = (0 << CS12) | (0 << CS11) | (0 << CS10); //Sets clock source to none effectively disabling timer1
+	TCCR3B = (0 << CS32) | (0 << CS31) | (0 << CS30); //Sets clock source to none effectively disabling timer1
 }
